c7c71a3d3eb4c24af7c18dbefca3171dbb37c7fe
Ryan Bartolome <ahhyann@icloud.com>
2025-02-21 09:52:48 -1000

    completed iteratative reverseList

--- src/reverse/reverse.js
+++ src/reverse/reverse.js
@@ -9,7 +9,30 @@ const LinkedList = require("../lib/linkedList");
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseIterative(list) {}
+
+/* 
+function reverseIterative(list)
+   // accepts list - a linked list, potentially empty, to be reversed
+
+   declare a variable named reversedList and initialize it to an empty LinkedList
+   declare a variable node initialized to the head of the list
+   while the node pointer is not null do
+     insert the node value at the head of the reversedList
+     move the node pointer along to the next node
+  return the reversedList
+*/
+function reverseIterative(list) {
+    if(!list.head) return null;
+
+    let reversedList = new LinkedList();
+    let node = list.head;
+
+    while(node){ 
+        reversedList.insertAtHead(node.value);
+        node = node.next;
+    }
+    return reversedList;
+}
 
 /**
  * Recursively reverse a linked list
@@ -20,6 +43,8 @@ function reverseIterative(list) {}
  * @returns {LinkedList}
  * A linked list in reverse order
  */
-function reverseRecursive(list) {}
+function reverseRecursive(list) {
+
+}
 
 module.exports = { reverseIterative, reverseRecursive };
